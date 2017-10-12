45248616a71c18055062a7271ceaea06a601f445
dominic.griesel <d.griesel@gmx.net>
2017-10-12 15:48:36 +0200

    setup typescript

---
+++ build/index.js
@@ -0,0 +1,2 @@
+console.log("Hello World");
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJzb3VyY2VSb290IjoiRDovdmlydHVhbC10c2Mvc3JjLyIsInNvdXJjZXMiOlsiaW5kZXgudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IkFBQUEsT0FBTyxDQUFDLEdBQUcsQ0FBQyxhQUFhLENBQUMsQ0FBQyJ9
